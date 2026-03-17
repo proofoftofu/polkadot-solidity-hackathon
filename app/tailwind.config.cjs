f9c0f223f49dd6d6da684cb640744fdab2dc625d
proofoftofu <proofoftofu@gmail.com>
2026-03-17 17:18:04 +1300

    fix tailwind

---
+++ app/tailwind.config.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  content: [
+    "./app/**/*.{js,jsx,ts,tsx,mdx}",
+    "./components/**/*.{js,jsx,ts,tsx,mdx}",
+    "./lib/**/*.{js,jsx,ts,tsx,mdx}"
+  ],
+  theme: {
+    extend: {}
+  },
+  plugins: []
+};
